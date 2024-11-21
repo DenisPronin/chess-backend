188080dff11e365c13b126a357d563adcff9af7b
DenisPronin <d.pronin@velvetech.dev>
2024-11-21 02:39:20 +0100

    Login

--- internal/user/user_handlers/login.go
+++ internal/user/user_handlers/login.go
@@ -1,15 +1,48 @@
 package user_handlers
 
 import (
-	"log"
+	"context"
+	"encoding/json"
+	"golang.org/x/crypto/bcrypt"
 	"net/http"
 )
 
 type LoginRequest struct {
 	Email    string `json:"email"`
+	Username string `json:"username"`
 	Password string `json:"password"`
 }
 
 func (h *UserHandler) Login(writer http.ResponseWriter, request *http.Request) {
-	log.Println("Login", request)
+	var req LoginRequest
+
+	err := json.NewDecoder(request.Body).Decode(&req)
+	if err != nil {
+		http.Error(writer, "Invalid request format", http.StatusBadRequest)
+		return
+	}
+
+	if req.Password == "" {
+		http.Error(writer, "Missing required fields", http.StatusBadRequest)
+		return
+	}
+
+	if req.Email == "" && req.Username == "" {
+		http.Error(writer, "Missing required fields", http.StatusBadRequest)
+		return
+	}
+
+	user, err := h.UserRepo.GetUserByUsernameOrEmail(context.Background(), req.Email, req.Username)
+	if err != nil {
+		http.Error(writer, "Invalid username/email or password", http.StatusUnauthorized)
+		return
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(req.Password)); err != nil {
+		http.Error(writer, "Invalid username/email or password", http.StatusUnauthorized)
+		return
+	}
+
+	writer.WriteHeader(http.StatusOK)
+	writer.Write([]byte("Login successful"))
 }
--- internal/user/user_repository.go
+++ internal/user/user_repository.go
@@ -18,3 +18,12 @@ func (rep *RepositoryUser) SaveUser(ctx context.Context, user User) error {
 	_, err := rep.DB.Exec(ctx, query, user.Email, user.Username, user.Password, user.CreatedAt)
 	return err
 }
+
+func (rep *RepositoryUser) GetUserByUsernameOrEmail(ctx context.Context, email string, username string) (User, error) {
+	query := `SELECT id, email, username, password, created_at FROM users WHERE email = $1 OR username = $2`
+	row := rep.DB.QueryRow(ctx, query, email, username)
+
+	var user User
+	err := row.Scan(&user.ID, &user.Email, &user.Username, &user.Password, &user.CreatedAt)
+	return user, err
+}
